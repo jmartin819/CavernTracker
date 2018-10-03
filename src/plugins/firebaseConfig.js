17c76137f97ec8584194b7dfc343d3b3799ad3a8
Josh Martin <josh.martin819@gmail.com>
2018-10-03 18:23:03 -0400

    Moved environmental variables to local file.

--- src/plugins/firebaseConfig.js
+++ src/plugins/firebaseConfig.js
@@ -4,12 +4,18 @@ import 'firebase/auth'
 
 // firebase init goes here
 const config = {
-    apiKey: "AIzaSyDBhXWwZID3pinI_X0AdHt2OlziQS9XBiI",
+/*    apiKey: "AIzaSyDBhXWwZID3pinI_X0AdHt2OlziQS9XBiI",
     authDomain: "turbodoto.firebaseapp.com",
     databaseURL: "https://turbodoto.firebaseio.com",
     projectId: "turbodoto",
     storageBucket: "turbodoto.appspot.com",
-    messagingSenderId: "416790912304"
+    messagingSenderId: "416790912304"*/
+    apiKey: process.env.VUE_APP_FIREBASE_APIKEY,
+    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+    projectId: process.env.VUE_APP_FIREBASE_PROJ_ID,
+    storageBucket: process.env.VUE_APP_FIREBASE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_FIREBASE_MSG_ID
 }
 firebase.initializeApp(config)
 
