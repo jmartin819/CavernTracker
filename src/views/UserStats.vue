e6fb3b02a4ae7b04b472a0f76bbb74bcaabc85e1
Josh Martin <josh.martin819@gmail.com>
2018-10-03 22:05:22 -0400

    Updated user stats page to display match based cards.

--- src/views/UserStats.vue
+++ src/views/UserStats.vue
@@ -5,8 +5,59 @@
         <input v-model="userID" placeholder="Enter UserID">
         <v-btn v-on:click="getUserStatsFromOpenDota(userID)">Lookup User</v-btn>
     </form>
-    <div>
-      Stats: {{ stats }}
+
+    <div v-for="item in stats">
+      <v-card v-if="item.player_slot > 127 && item.radiant_win == 0" class="green lighten-3">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Match: {{item.match_id}}</h3>
+            <li>Hero Played: {{ item.hero_id }}</li>
+            <li>Duration: {{ item.duration }}</li>
+            <li>Kills: {{ item.kills }}</li>
+            <li>Assists: {{ item.assists }}</li>
+            <li>Deaths: {{ item.deaths }}</li>
+          </div>
+        </v-card-title>
+      </v-card>
+
+      <v-card v-else-if="item.player_slot > 127 && item.radiant_win == 1" class="red lighten-3">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Match: {{item.match_id}}</h3>
+            <li>Hero Played: {{ item.hero_id }}</li>
+            <li>Duration: {{ item.duration }}</li>
+            <li>Kills: {{ item.kills }}</li>
+            <li>Assists: {{ item.assists }}</li>
+            <li>Deaths: {{ item.deaths }}</li>
+          </div>
+        </v-card-title>
+      </v-card>
+
+      <v-card v-else-if="item.player_slot <= 127 && item.radiant_win == 0" class="red lighten-3">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Match: {{item.match_id}}</h3>
+            <li>Hero Played: {{ item.hero_id }}</li>
+            <li>Duration: {{ item.duration }}</li>
+            <li>Kills: {{ item.kills }}</li>
+            <li>Assists: {{ item.assists }}</li>
+            <li>Deaths: {{ item.deaths }}</li>
+          </div>
+        </v-card-title>
+      </v-card>
+
+      <v-card v-else class="green lighten-3">
+        <v-card-title primary-title>
+          <div>
+            <h3 class="headline mb-0">Match: {{item.match_id}}</h3>
+            <li>Hero Played: {{ item.hero_id }}</li>
+            <li>Duration: {{ item.duration }}</li>
+            <li>Kills: {{ item.kills }}</li>
+            <li>Assists: {{ item.assists }}</li>
+            <li>Deaths: {{ item.deaths }}</li>
+          </div>
+        </v-card-title>
+      </v-card>
     </div>
   </div>
 
